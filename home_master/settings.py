2e6d78f3dd8cd0ecae5548497808d786ff3ebe68
GrigoryKalinin <GrigoryKalinin@mail.ru>
2025-07-14 15:30:40 +0500

    Добавление моделей Category и Product с миграцией, обновление настроек приложения

--- home_master/settings.py
+++ home_master/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+        
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -47,8 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-    'main',
 ]
 
 ROOT_URLCONF = 'home_master.urls'
--- main/models.py
+++ main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True) # название категории
+    slug = models.SlugField(max_length=100, unique=True) # человекочитаемый URL 
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return str(self.name)
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE) # связь с категорией
+    name = models.CharField(max_length=100, db_index=True) # название товара
+    slug = models.SlugField(max_length=100, unique=True) # человекочитаемый URL
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True) # изображение товара
+    description = models.TextField(blank=True) # описание товара
+    price = models.DecimalField(max_digits=10, decimal_places=2) # цена товара
+    available = models.BooleanField(default=True) # доступность товара
+    date_created = models.DateTimeField(auto_now_add=True) # дата создания товара
+    date_update = models.DateTimeField(auto_now=True) # дата обновления товара
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return str(self.name)
