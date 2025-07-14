c076cc8ff4604a13e6f5d681c7fe4d077822217e
GrigoryKalinin <GrigoryKalinin@mail.ru>
2025-07-14 15:51:37 +0500

    Добавление моделей Category и Product в админку с настройками отображения и фильтрации

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug'] # поля, которые будут отображаться в админке
+    prepopulated_fields = {'slug': ('name',)} # автоматическое заполнение поля slug по полю name
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display =  ['name', 'category', 'price', 'available', 'date_created', 'date_update'] # поля, которые будут отображаться в админке
+    list_filter = ['available', 'date_created', 'date_update', 'category'] # поля, по которым будет фильтрация
+    list_editable = ['price', 'available'] # поля, которые можно редактировать
+    prepopulated_fields = {'slug': ('name',)} # автоматическое заполнение поля slug по полю name
