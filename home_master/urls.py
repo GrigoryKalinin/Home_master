7a7c4e06696b5cc892cd491039f272f7d01f58c8
GrigoryKalinin <GrigoryKalinin@mail.ru>
2025-07-28 15:10:49 +0500

    - Создано приложение user, настроены базовые urls и settings

--- home_master/urls.py
+++ home_master/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
