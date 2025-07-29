b6d5afd8d372e554af4bb693dd5ac317757c18a3
GrigoryKalinin <GrigoryKalinin@mail.ru>
2025-07-29 14:51:54 +0500

    - Миграция для суперпользователя. Форма и модели для регистрации

---
+++ users/forms.py
@@ -0,0 +1,125 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.html import strip_tags
+
+from phonenumber_field.formfields import PhoneNumberField
+
+User = get_user_model()
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ваш Email'})
+    )
+    first_name = forms.CharField(
+        label='Имя',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ваше имя'})
+    )
+    last_name = forms.CharField(
+        label='Фамилия',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ваша фамилия'})
+    )
+    phone = PhoneNumberField(
+        region="RU",
+        label="Ваш телефон",
+        widget=forms.TextInput(attrs={"placeholder": "+7 (999) 123-45-67", "class": "form-control phone-mask", "data-mask": "+7 (000) 000-00-00", "autocomplete": "off",  "inputmode": "tel"}),
+    )
+    password1 = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'})
+    )
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Подтверждение пароля'})
+    )
+    marketing_consent1 = forms.BooleanField(label='Согласие на рассылку', required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
+
+    marketing_consent2 = forms.BooleanField(label='Согласие на обработку персональных данных', required=True, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password1', 'password2', 'marketing_consent1', 'marketing_consent2')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Пользователь с таким email уже существует')
+        return email
+    
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if phone and User.objects.filter(phone=phone).exists():
+            raise forms.ValidationError('Пользователь с таким телефоном уже существует')
+        return phone
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.marketing_consent1 = self.cleaned_data['marketing_consent1']
+        user.marketing_consent2 = self.cleaned_data['marketing_consent2']
+        if commit:
+            user.save()
+        return user
+    
+
+class CustomUserLoginForm(AuthenticationForm):
+    username = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ваш Email', 'autocomplete': 'email', 'autofocus': True})
+    )
+    password = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'})
+    )
+
+    def clean(self):
+        email = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if email and password:
+            try:
+                self.user_cache = authenticate(self.request, email=email, password=password)
+                if self.user_cache is None:
+                    raise forms.ValidationError('Неверный логин или пароль')
+                self.confirm_login_allowed(self.user_cache)
+            except forms.ValidationError:
+                raise
+        return self.cleaned_data
+    
+class CustomUserChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'phone', 'city', 'address', 'avatar')
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Фамилия'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Телефон'}),
+            'city': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Город'}),
+            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Адрес'}),
+            'avatar': forms.FileInput(attrs={'class': 'form-control', 'placeholder': 'Аватар'}),
+        }
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if email and User.objects.filter(email=email).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError('Пользователь с таким email уже существует')
+        return email
+    
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if phone and User.objects.filter(phone=phone).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError('Пользователь с таким телефоном уже существует')
+        return phone
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        if not cleaned_data.get('email'):
+            cleaned_data['email'] = self.instance.email
+
+        for field in ['first_name', 'last_name', 'city', 'address']:
+            if field in cleaned_data and cleaned_data[field]:
+                cleaned_data[field] = strip_tags(str(cleaned_data.get(field, '')))
+
+        return cleaned_data
+
--- users/models.py
+++ users/models.py
@@ -1,7 +1,30 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.html import strip_tags
 from django.db import models
 
 from phonenumber_field.modelfields import PhoneNumberField
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Введите свой email')
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Супер юзер должен иметь is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Супер юзер должен иметь is_superuser=True.')
+        
+        return self.create_user(email, first_name, last_name, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -10,12 +33,17 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=150,verbose_name='Фамилия', db_index=True)
     phone = PhoneNumberField(verbose_name="Телефон", blank=True, db_index=True, region="RU")
     city = models.CharField(max_length=100, blank=True, verbose_name='Город')
+    address = models.CharField(max_length=200, blank=True, verbose_name='Адрес')
     avatar = models.ImageField(upload_to='users/avatars/', blank=True, null=True, verbose_name='Аватар')
-    
+    marketing_consent1 = models.BooleanField(default=False, verbose_name='Согласие на рассылку')
+    marketing_consent2 = models.BooleanField(default=False, verbose_name='Согласие на обработку персональных данных')
+    username = None
+
+    objects = CustomUserManager()
 
     # Настройки аутентификации
     USERNAME_FIELD = 'email'  # Используем email для входа
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']  # Обязательные поля при createsuperuser
+    REQUIRED_FIELDS = ['first_name', 'last_name']  # Обязательные поля при createsuperuser
 
     class Meta:
         verbose_name = 'Пользователь'
@@ -30,3 +58,4 @@ class User(AbstractUser):
 
     def get_short_name(self):
         return self.first_name
+    
