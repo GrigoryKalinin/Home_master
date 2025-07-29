53f26593e053ab59266ae3a8a2f5c70f91fc135e
GrigoryKalinin <GrigoryKalinin@mail.ru>
2025-07-29 16:13:27 +0500

    - Представления и урлы для регистрации, входа и профиля

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, FormView, TemplateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm, CustomUserLoginForm, CustomUserChangeForm
+from .models import User
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:profile')
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
+
+
+class LoginView(FormView):
+    form_class = CustomUserLoginForm
+    template_name = 'users/login.html'
+    success_url = reverse_lazy('users:profile')
+    
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
+    
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return super().form_valid(form)
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'users/profile.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
