b3519814eb929a527710cf6ab26258d13c3f85ef
GrigoryKalinin <GrigoryKalinin@mail.ru>
2025-08-01 13:27:54 +0500

    - Настроено модальное окно заявки в шаблонах карьеры

--- main/views.py
+++ main/views.py
@@ -37,6 +37,7 @@ class JobApplicationView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Работа в МастерГранд"
+        context["order_from"] = OrderForm()
         return context
 
 # Категории (для сотрудников)
